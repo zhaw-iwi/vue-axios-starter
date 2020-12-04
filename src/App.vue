81454bdbcd4af844886f6dbc97e10d44ba737ac4
Max Meisterhans <max.meisterhans@gmail.com>
2020-12-04 15:22:40 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <router-link class="navbar-brand" to="/">Infections DB</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/demo">Demo</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/persons">Persons</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/pathogens">
+              Pathogens
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/infections">
+              Infections
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style>
+#navbarNav .router-link-active {
+  color: #a70909;
+}
+</style>
---
+++ src/views/Persons.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1 class="mt-3">List of all Persons</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Birthdate</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="person.id" v-for="person in persons">
+          <td>
+            <router-link v-bind:to="'/persons/' + person.id">
+              {{ person.id }}
+            </router-link>
+          </td>
+          <td>
+            {{ person.name }}
+          </td>
+          <td>
+            {{ person.birthdate }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <router-link to="/create-person">+ Add Person</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      persons: [
+        {
+          id: 1,
+          name: "Myles Popley",
+          birthdate: 372019027,
+        },
+        {
+          id: 2,
+          name: "Reine Steers",
+          birthdate: 1056149025,
+        },
+      ],
+    };
+  },
+};
+</script>
